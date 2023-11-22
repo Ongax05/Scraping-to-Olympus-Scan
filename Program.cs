dd9a7856539603b3525df84ef5285cae21a081c7
Ongax <jhoseppulido1129@gmail.com>
2023-11-22 14:02:25 -0500

    feat: :construction: Trying imgs to pdf

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using System.Net;
 using WebScraping;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var Url = "https://olympusv2.gg/capitulo/39894/comic-helmut-el-nino-abandonado";
-        var Links = Scraping.GetLinks(Url);
+//        var Url = "https://olympusv2.gg/capitulo/39894/comic-helmut-el-nino-abandonado";
+
+        var ImgsPath = "./Images/";
+        if (!Path.Exists(ImgsPath))
+            Directory.CreateDirectory(ImgsPath);
+
+        var PdfsPath = "./Pdfs/";
+        if (!Path.Exists(PdfsPath))
+            Directory.CreateDirectory(PdfsPath);
+
+//        await Scraping.DownloadImgsAsync(Url, ImgsPath);
+        Scraping.CreatePDF(ImgsPath, PdfsPath, "cap1.pdf");
     }
 }
--- WebScraping.cs
+++ WebScraping.cs
@@ -1,5 +1,6 @@
-using System.Text.RegularExpressions;
 using HtmlAgilityPack;
+using Aspose.Pdf;
+using Aspose.Imaging;
 
 namespace WebScraping
 {
@@ -10,9 +11,11 @@ namespace WebScraping
         public static List<string> GetLinks(string url)
         {
             var document = Web.Load(url);
-            var imgNodes = document.DocumentNode.SelectNodes(
-                "//img[contains(@class, 'object-cover rounded-inherit w-full h-full')]"
-            );
+            var imgNodes = document
+                .DocumentNode
+                .SelectNodes(
+                    "//img[contains(@class, 'object-cover rounded-inherit w-full h-full')]"
+                );
             string Img1 = imgNodes.First().GetAttributeValue("src", "").ToString();
             string Nombre = Img1[..^8];
             var Links = imgNodes.Select(n => n.GetAttributeValue("src", ""));
@@ -23,10 +26,52 @@ namespace WebScraping
         public static string NextLink(string url)
         {
             var document = Web.Load(url);
-            var NextCapLink = document.DocumentNode
+            var NextCapLink = document
+                .DocumentNode
                 .SelectNodes("//a")
-                .Where(n => n.GetAttributeValue("name", "").ToString() == "capitulo siguiente").FirstOrDefault();
-            return "https://olympusv2.gg" + NextCapLink.GetAttributeValue("href","");
+                .Where(n => n.GetAttributeValue("name", "").ToString() == "capitulo siguiente")
+                .FirstOrDefault();
+            return "https://olympusv2.gg" + NextCapLink.GetAttributeValue("href", "");
+        }
+
+        public static async Task DownloadImgsAsync(string url, string ImgsPath)
+        {
+            var Client = new HttpClient();
+
+            var Links = GetLinks(url);
+
+            for (int i = 0; i < Links.Count; i++)
+            {
+                var uri = new Uri(Links[i]);
+                var fileName = $"{ImgsPath}{i}.jpg";
+
+                var response = await Client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    //using var stream = ConvertImageToJpeg(new FileStream(fileName, FileMode.Create));
+                    //await response.Content.CopyToAsync(stream);
+                }
+            }
+        }
+
+        public static List<string> GetImgNames(string ImgsPath)
+        {
+            var Directory = new DirectoryInfo(ImgsPath);
+            var r = Directory.GetFiles().Select(f => f.Name).OrderBy(i=> int.Parse(i[..^4])).ToList();
+            return r;   
+        }
+
+        public static void CreatePDF (string ImgsPath, string PdfsPath, string PdfName)
+        {
+            Document pdfDocument = new();
+            var Imgs = GetImgNames(ImgsPath);
+            for (int i = 0; i < Imgs.Count; i++)
+            {
+                FileStream ImageStream = new(ImgsPath + Imgs[i], FileMode.Open);
+                pdfDocument.Pages.Add();
+                pdfDocument.Pages[i+1].Resources.Images.Add(ImageStream);
+            }
+            pdfDocument.Save(PdfsPath + PdfName);
         }
     }
 }
